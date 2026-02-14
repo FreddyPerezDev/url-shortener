0b6983af7d22abd372dea3f4a0d3b1e639879975
Freddy Pérez <freddyp.dev@gmail.com>
2026-02-13 18:43:07 -0600

    feat: agregar soporte para despliegue en Vercel en la configuración de Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,16 @@
 // @ts-check
-
-import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
-	output: "server",
-	adapter: node({
-		mode: "standalone",
-	}),
-	vite: {
-		plugins: [tailwindcss()],
+  output: "server",
+
+  vite: {
+      plugins: [tailwindcss()],
 	},
+
+  adapter: vercel(),
 });
